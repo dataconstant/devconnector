fd5b622458c0ea81bd1edaa5d5c2792b0ff5c1cc
dataconstant <abhishekchetri@outlook.com>
2020-01-06 16:18:05 +1100

    profile experience done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -107,4 +107,80 @@ router.get('/', async (req, res) => {
     }
   });
 
+
+  router.delete('/', auth, async (req, res) => {
+    try {
+      await Profile.findOneAndRemove({user:req.user.id});
+      await User.findOneAndRemove({_id:req.user.id});
+      res.json({'msg':'User Deleted'});
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
+
+
+
+  router.put('/experience', [auth,[
+    check('title','Title is Required').not().isEmpty(),
+    check('company','Company is Required').not().isEmpty(),
+    check('from','From Date is Required').not().isEmpty()
+  ]], async(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors:errors.array()});
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title, 
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    }
+
+    try{
+      const profile = await Profile.findOne({user:req.user.id});
+      profile.experience.unshift(newExp);
+      await profile.save();
+
+      res.json(profile);
+
+    }catch(error){
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+
+  });
+
+  router.delete('/experience/:exp_id', auth, async(req,res)=>{
+    try{
+      const profile = await Profile.findOne({user:req.user.id});
+
+      const removeIndex = profile.experience.map(item=>item.id).indexOf(req.params.exp_id);
+
+      profile.experience.splice(removeIndex,1);
+      await profile.save();
+      res.json(profile);
+
+    }catch(error){
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  })
+
+
+
 module.exports = router;
